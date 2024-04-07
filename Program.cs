86b58e927331610cd24ba36e48bbe346282cf612
felipe <165731375+felipelopes03@users.noreply.github.com>
2024-04-07 16:41:46 -0300

    bumerangue

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double percentualBonus;
+double percentualRetorno;
+int pontosTransferidos;
+double pontosReduzir;
+double pontosAcrescentar;
+
+Console.WriteLine("--- Milhas Bumerangue ---\n");
+
+Console.Write("Digite o percentual de bônus (em %): ");
+percentualBonus = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o percentual de retorno (em %): ");
+percentualRetorno = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a quantidade de pontos a serem transferidos: ");
+pontosTransferidos = Convert.ToInt32(Console.ReadLine());
+
+pontosReduzir = Convert.ToInt32(pontosTransferidos - (percentualRetorno / 100) * pontosTransferidos); 
+pontosAcrescentar = Convert.ToInt32(pontosTransferidos + (percentualBonus / 100) * pontosTransferidos); 
+
+Console.WriteLine($"\nQuantidade de pontos a reduzir na origem: {pontosReduzir}");
+Console.WriteLine($"Quantidade de pontos a acrescentar no destino: {pontosAcrescentar}");
